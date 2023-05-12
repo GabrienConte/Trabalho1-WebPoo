5b8ce708407368777b824f81ff8e6fb9e3facb4d
GabrienConte <gabrielconte0@gmail.com>
2023-05-11 21:19:15 -0300

    "DAO, MODEL, CONNECT DB e Teste feitos"

--- src/main/java/teste/ClienteTeste.java
+++ src/main/java/teste/ClienteTeste.java
@@ -1,4 +1,17 @@
 package teste;
 
+import dao.ClienteDao;
+import model.Cliente;
+
 public class ClienteTeste {
+    public static void main(String[] args) {
+        Cliente c = new Cliente("Allan3","11111");//cria um objeto cliente
+        Cliente c1 = new Cliente("Rodrigues", "000.000.000-45");
+        ClienteDao dao = new ClienteDao();//cria um objeto dao para acessar os métodos de manipulação do banco
+        // dao.salvar(c);//salva o objeto cliente no banco de dados
+        //dao.deletar(7);
+        //dao.atualizar(c1,8);
+
+
+    }
 }
