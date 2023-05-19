b5eb2024cf00d2c5a214f5040ab54f946d57c57f
Aluno <gabrielconte0@gmail.com>
2023-05-19 15:42:21 -0300

    login e página principal.jsp feita

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,5 +15,19 @@ public class LoginController extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         /* super.service(req, resp); */
         System.out.println("bateu no login");
+
+        String login = req.getParameter("name");
+        String password = req.getParameter("password");
+
+        RequestDispatcher dispatcher;
+        if(login.equals("conte") && password.equals("qwe")){
+            System.out.println("Logado");
+            dispatcher = req.getRequestDispatcher("/WEB-INF/principal.jsp");
+        }else{
+            System.out.println("deu ruim");
+            dispatcher = req.getRequestDispatcher("/index.jsp");
+        }
+
+        dispatcher.forward(req, resp);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
@@ -11,9 +8,12 @@
 <div>
     <form method="get" action="login" >
         <label for="name">Nome:</label>
-        <input type="text" id="name" name="name">
+            <input type="text" id="name" name="name">
+
         <label for="password">Senha:</label>
-        <input type="text" id="password" name="password">
+            <input type="text" id="password" name="password">
+        <br>
+        <br>
         <button type="submit">Autenticar</button>
     </form>
 </div>
